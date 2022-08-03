cdba2125cd09c80313a77bd9a5152fc5f0bc6486
Hakan CERAN <hakanceran64@gmail.com>
2022-08-03 16:19:57 +0300

    structs

---
+++ Classes/Structs/structs.cpp
@@ -0,0 +1,29 @@
+/*
+ * @Author: Hakan CERAN
+ * @Time: 03.08.2022
+ * @Content: Strings
+ * 
+ * */
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct Student {
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
+};
+
+int main() {
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
+    return 0;
+}
